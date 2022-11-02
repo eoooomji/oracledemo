47727eb2bd3ad353adf60a87ec28e87b75a793e2
EomJiChan <akcks101@gmail.com>
2022-11-02 23:08:04 +0900

    update | 20221102

--- oracle문제4_subquery.sql
+++ oracle문제4_subquery.sql
@@ -51,17 +51,63 @@ WHERE job_id = ( SELECT job_id
                  WHERE employee_id = 177);
   
 6) 최소 급여를 받는 사원의 이름(first_name), 담당 업무(job_id) 및 급여(salary)를 표시하시오(그룹함수 사용).
-
+SELECT first_name, job_id, salary
+FROM employees
+WHERE salary = (SELECT min(salary)
+                 FROM employees);
 				
 7)업무별 평균 급여가 가장 적은  업무(job_id)를 찾아 업무(job_id)와 평균 급여를 표시하시오.
-
+SELECT job_id, avg(salary)
+FROM employees
+GROUP BY job_id
+HAVING avg(salary) = (SELECT min(avg(salary))
+                      FROM employees
+                      GROUP BY job_id);
+--SELECT job_id, min(avg(salary))
+--FROM employees
+--GROUP BY job_id;
 					  
 8) 각 부서의 최소 급여를 받는 사원의 이름(first_name), 급여(salary), 부서번호(department_id)를 표시하시오.
+SELECT first_name, salary, department_id
+FROM employees
+WHERE (department_id, salary) IN (SELECT department_id, min(salary)
+                                  FROM employees
+                                  GROUP BY department_id)
+ORDER BY department_id;
 
+SELECT first_name, salary, department_id
+FROM employees e1
+WHERE salary = (SELECT min(salary)
+                FROM employees e2
+                WHERE e1.department_id = e2.department_id)
+ORDER BY department_id;
 
 9)담당 업무가 프로그래머(IT_PROG)인 모든 사원보다 급여가 적으면서 
 업무가 프로그래머(IT_PROG)가 아닌  사원들의 사원번호(employee_id), 이름(first_name), 
 담당 업무(job_id), 급여(salary))를 출력하시오.
-           
+SELECT employee_id, first_name, job_id, salary
+FROM employees
+WHERE job_id != 'IT_PROG'
+AND salary < ALL (SELECT salary
+                  FROM employees
+                  WHERE job_id = 'IT_PROG')
+ORDER BY employee_id;
 
 10)부하직원이 없는 모든 사원의 이름을 표시하시오.
+SELECT employee_id, first_name
+FROM employees
+WHERE employee_id NOT IN (SELECT e.employee_id
+                          FROM employees e, employees m
+                          WHERE e.manager_id = m.manager_id;)
+ORDER BY employee_id;
+
+SELECT employee_id, first_name
+FROM employees e
+WHERE NOT EXISTS (SELECT employee_id
+                  FROM employees m
+                  WHERE e.manager_id = m.manager_id)
+ORDER BY employee_id;
+
+SELECT employee_id, first_name
+FROM employees
+WHERE manager_id IS NULL;
